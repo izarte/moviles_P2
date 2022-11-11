cd1b7c5458b56d0cbf112868f87a0c843ab8afd9
Victoria293 <victoria.nik007@gmail.com>
2022-11-11 15:20:45 +0100

    add vicki files with real tests

--- catkin_ws/build/marker_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/marker_msgs
-# Build directory: /catkin_ws/build/marker_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
